31f86bb69fcd9dd455c66e62d35ecfd150406478
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-02-27 18:10:38 +0100

    header cart

---
+++ wp-content/themes/mytheme/hooks.php
@@ -0,0 +1,24 @@
+    
+<?php
+/* *******CART******* */
+
+//change text on proceed to checkout btn
+function woocommerce_button_proceed_to_checkout() {
+	
+    $new_checkout_url = WC()->cart->get_checkout_url();
+    ?>
+    <a href="<?php echo $new_checkout_url; ?>" class="checkout-button button alt wc-forward">
+    
+    <?php _e( 'Checkout', 'woocommerce' ); ?></a>
+    
+<?php
+}
+
+//removes shipping 
+    add_filter( 'woocommerce_cart_needs_shipping', 'filter_cart_needs_shipping' );
+    function filter_cart_needs_shipping( $needs_shipping ) {
+        if ( is_cart() ) {
+            $needs_shipping = false;
+        }
+        return $needs_shipping;
+    }
