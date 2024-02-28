7c78ce0a729345c8d99bdd45aa6d8400ce52b682
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-02-28 14:33:06 +0100

    cart

--- wp-content/themes/mytheme/hooks.php
+++ wp-content/themes/mytheme/hooks.php
@@ -13,18 +13,38 @@ function woocommerce_button_proceed_to_checkout() {
 <?php
 }
 
-//removes shipping 
+/* //removes shipping 
     add_filter( 'woocommerce_cart_needs_shipping', 'filter_cart_needs_shipping' );
     function filter_cart_needs_shipping( $needs_shipping ) {
         if ( is_cart() ) {
             $needs_shipping = false;
         }
         return $needs_shipping;
-    }
+    } */
 
-    add_filter('woocommerce_breadcrumb_defaults', 'custom_change_breadcrumb_separator');
+
+// Hook för att lägga till beräknad skatt i kundvagnen och kassan
+add_action( 'woocommerce_cart_totals_before_shipping', 'display_estimated_tax', 20 );
+/* add_action( 'woocommerce_review_order_before_order_total', 'display_estimated_tax', 20 );
+ */
+function display_estimated_tax() {
+    $cart_tax = WC()->cart->get_cart_contents_tax();
+    if ( $cart_tax > 0 ) {
+        ?>
+        <tr class="order-tax">
+            <th><?php _e( 'Estimated Tax', 'your-textdomain' ); ?></th>
+            <td data-title="<?php esc_attr_e( 'Estimated Tax', 'your-textdomain' ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
+           
+        </tr>
+        <?php
+    }
+}
+
+
+add_filter('woocommerce_breadcrumb_defaults', 'custom_change_breadcrumb_separator');
 
     function custom_change_breadcrumb_separator($defaults) {
         $defaults['delimiter'] = '<span> > </span>';
         return $defaults;
     }
+
